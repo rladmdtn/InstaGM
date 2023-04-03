b041fe374dcdecdd65a38b2793cdec0c6d599121
rladmdtn <rladmdtn0209@naver.com>
2023-04-03 17:12:54 +0900

    2023.04.03 js추가

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+document.addEventListener("DOMContentLoaded",function(e){
+    let btn = document.querySelector("#mybtn");
+
+
+btn.addEventListener("mouseover",function(e){
+  e.target.style.backgroundColor = "#808080";
+ });
+btn.addEventListener("mouseout",function(e){
+  e.target.style.backgroundColor = "#ffffff";
+
+ });
+
+let icn = document.querySelector("#fa-icon");
+
+icn.addEventListener("mouseover", (e) => {
+    e.target.setAttribute("class","far fa-cog fa-spin");
+});
+icn.addEventListener("mouseout", (e) => {
+    e.target.setAttribute("class","far fa-cog ");
+});
+
+home.addEventListener("mouseover", (e) => {
+    e.target.style.color = "#000000";
+});
+home.addEventListener("mouseout", (e) => {
+    e.target.style.color = "#808080";
+});
+
+
+sub_icons.addEventListener("mouseover", (e) => {
+    e.target.style.color = "#000000";
+});
+sub_icons.addEventListener("mouseout", (e) => {
+    e.target.style.color = "#808080";
+});
+profile_pic.addEventListener("click",function(e){
+   
+
+    });
+});
